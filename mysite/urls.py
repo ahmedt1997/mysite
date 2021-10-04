e221774a551018513d4a94a894abbb86aa416fec
Ahmed Touzani <ahmedbbccaa@gmail.com>
2021-10-04 05:55:24 +0100

    Adding a sitemap to your site

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
+from django.contrib.sitemaps.views import sitemap
 from django.urls import path, include
+
+from blog.sitemaps import PostSitemap
+
+sitemaps = {
+    'posts': PostSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # pour afficher la page login au debut de chaque demarage
     # path('', admin.site.urls),
     path('blog/', include('blog.urls', namespace='blog')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
+         name='django.contrib.sitemaps.views.sitemap')
 ]
